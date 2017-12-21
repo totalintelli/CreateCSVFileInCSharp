db28f64eaac10dac46b6e83a12e75006df637375
DEVZONEYDS\USER <kwanwoo5@naver.com>
2017-12-21 21:09:27 +0900

    first commit

---
+++ CreateCSVFileInCSharp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CreateCSVFileInCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder csvcontent = new StringBuilder();
+            csvcontent.AppendLine("Name,Age");
+            csvcontent.AppendLine("Mahesh,31");
+            string csvpath = "D:\\xyz.csv";
+            File.AppendAllText(csvpath, csvcontent.ToString());
+        }
+    }
+}
